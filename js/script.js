89d2943507520423cf5377666c313c00745ddebb
Abram <abramperez@ifca.unican.es>
2026-03-21 11:03:41 +0100

    Change one-page format

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
 	// OWL CAROUSEL INSTALLATION
 	$("#testimonial-carousel").owlCarousel({
 		items:1,
-		itemsDesktop : [1000,1], //5 items between 1000px and 901px
-      	itemsDesktopSmall : [900,1], // betweem 900px and 601px
+		itemsDesktop : [1000,1],
+      	itemsDesktopSmall : [900,1],
       	itemsTablet: [600,1],
 		itemsMobile :[479,1],
 		pagination:true
 	});
+
 	$("#home-slider").owlCarousel({
 		items:1,
-		itemsDesktop : [1000,1], //5 items between 1000px and 901px
-      	itemsDesktopSmall : [900,1], // betweem 900px and 601px
+		itemsDesktop : [1000,1],
+      	itemsDesktopSmall : [900,1],
       	itemsTablet: [600,1],
 		itemsMobile :[479,1],
 		pagination:false,
 		navigation:true,
 		navigationText:["<i class='ion-ios-arrow-left'></i>","<i class='ion-ios-arrow-right'></i>"]
-
 	});
 
 	/* Navigation Menu*/
@@ -32,7 +32,8 @@ $(document).ready(function() {
         $('.navbar').removeClass('gradient-violat');
         $("#scroll-top-div").fadeOut('500');
     }
-	var num = 50; //number of pixels before modifying styles
+
+	var num = 50;
 
 	$(window).bind('scroll', function () {
 	    if ($(window).scrollTop() > num) {
@@ -46,32 +47,6 @@ $(document).ready(function() {
 	    }
 	});
 
-
-
-	/* SMOOTH SCROLLING SCRIPT*/
-	// Add smooth scrolling to all links
-	$(".navbar-nav li a").on('click', function(event) {
-
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
-
-			// Store hash
-			var hash = this.hash;
-
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 800, function(){
-
-				// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-			});
-		} // End if
-	});
-
 	/****************************BACK TO TOP************************************/
 	$('#scroll-top-div').on('click', function (e) {
         e.preventDefault();
@@ -79,5 +54,5 @@ $(document).ready(function() {
             scrollTop: 0
         }, 700);
     });
- 
+
 });
